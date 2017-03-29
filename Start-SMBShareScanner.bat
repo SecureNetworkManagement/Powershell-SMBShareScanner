fbc7688c658fe76fa103904dc6002ba924ee992c
Secure Network Management <contact@securenetworkmanagement.com>
2017-03-29 09:40:25 -0600

    Update Start-SMBShareScanner.bat

--- Start-SMBShareScanner.bat
+++ Start-SMBShareScanner.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Echo OFF
 set /P Domain=Enter Domain:
 set /P Usern=Enter Username:
@@ -6,13 +5,4 @@ set /P Usern=Enter Username:
 runas /user:%Domain%\%Usern% " powershell -nologo -noprofile -noexit  -Command '%~dp0SMBShareScanner.ps1'
 
 cls
-=======
-Echo OFF
-set /P Domain=Enter Domain:
-set /P Usern=Enter Username:
- 
-runas /user:%Domain%\%Usern% " powershell -nologo -noprofile -noexit  -Command '%~dp0SMBShareScanner.ps1'
-
-cls
->>>>>>> 6577e8c9ed0db5c2060cf857961cd0bfb99eeda5
 exit
